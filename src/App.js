2732ab1544c0daffe6d7fb5e7d5a6c3b0ccd88a7
Apple <apple@Apples-MacBook-Pro-526.local>
2021-09-20 19:48:02 +0400

    updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Solutions from './Solutions.js';
 import Docs from './Docs.js';
 import Contacts from './Contacts.js';
 import GetScreen from '../src/SubComps/GetScreen';
+
 const App = () => {
   return (
     <div>
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/docs" exact={true} component={Docs} />
         <Route path="/contacts" exact={true} component={Contacts} />
         <Route path="/get started" exact={true} component={GetScreen} />
+   
         
         
     
